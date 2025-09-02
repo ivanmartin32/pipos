32642e01f4e301c87c8edcf85a35751e4e478277
ivanmartin32 <ivanmartin_32@hotmail.com>
2025-09-01 21:20:45 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,48 @@
+function mostrar_comidas() {
+    document.getElementById("p1").innerHTML = 
+    "<h3> CARTA DE COMIDAS </h3>" +
+    "<h5> Empanadas </h5>" +
+    "<p> - Carne    $650 <br> - Jamón y Queso   $650</p>" +
+    "<h5> Pizzas </h5>" +
+    "<p> - Muzza    $1200 <br> - Napolitana   $1300 <br> - Fugazzeta   $1400 </p>" +
+    "<h5> Hamburguesas </h5>" +
+    "<p> - Simple   $1100 <br> - Doble   $1300 <br> - Triple   $1500 </p>" +
+    "<h5> Ensaladas </h5>" +
+    "<p> - César    $900 <br> - Mixta   $800 <br> - Rusa   $850 </p>" +
+    "<h5> Pastas </h5>" +
+    "<p> - Tallarines   $1000 <br> - Ravioles   $1200 <br> - Ñoquis   $1100 </p>" +
+    "<h5> Carnes </h5>" +
+    "<p> - Bife de Chorizo   $2000 <br> - Ojo de Bife   $2200 <br> - Entraña   $2100 </p>" +
+    "<h5> Acompañamientos </h5>" +
+    "<p> - Papas Fritas   $500 <br> - Puré   $400 <br> - Verduras   $450 </p>";
+}
+function mostrar_bebidas() {
+    document.getElementById("p1").innerHTML = 
+    "<h3> CARTA DE BEBIDAS </h3>" +
+    "<h5> Gaseosas </h5>" +
+    "<p> - Coca Cola  $300 <br> - Sprite    $300 </p>" +
+    "<h5> Jugos </h5>" +
+    "<p> - Naranja   $350 <br> - Manzana   $350 </p>" +
+    "<h5> Cervezas </h5>" +
+    "<p> - Heineken  $400 <br> - Stella Artois  $400 </p>" + 
+    "<h5> Vinos </h5>" +
+    "<p> - Malbec    $500 <br> - Cabernet Sauvignon  $500 </p>" +
+    "<h5> Aguas </h5>" +
+    "<p> - Con gas   $250 <br> - Sin gas  $250 </p>" +
+    "<h5> Tragos </h5>" +
+    "<p> - Gancia   $600 <br> - Fernet  $600 </p>";
+}   
+function mostrar_postres() {
+    document.getElementById("p1").innerHTML = 
+    "<h3> CARTA DE POSTRES </h3>" +
+    "<h5> Helados </h5>" +
+    "<p> - Vainilla   $400 <br> - Chocolate   $400 <br> - Dulce de Leche   $400 </p>" +
+    "<h5> Tortas </h5>" +
+    "<p> - Chocolate   $500 <br> - Red Velvet   $550 <br> - Cheesecake   $600 </p>" +
+    "<h5> Flanes </h5>" +
+    "<p> - Flan con Dulce de Leche   $350 <br> - Flan con Crema   $350 </p>" +
+    "<h5> Postres al Plato </h5>" +
+    "<p> - Brownie con Helado   $700 <br> - Panqueques con Dulce de Leche   $650 </p>" +
+    "<h5> Frutas </h5>" +
+    "<p> - Ensalada de Frutas   $300 <br> - Macedonia   $300 </p>";
+}
